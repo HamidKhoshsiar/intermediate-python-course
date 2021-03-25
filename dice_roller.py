a34191f208cd7ba54a1c9e27c4d6abcb0828e5ab
HamidKhoshsiar <59198898+HamidKhoshsiar@users.noreply.github.com>
2021-03-26 00:36:42 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
 def main():
   #print('You rolled a die')
+  dice = random.randrange(1, 7)
+
+  if dice == 6:
+    dice = random.randrange(1, 7)
+    print(dice)
+  else:
+    print(dice)
 
 if __name__== "__main__":
   main()
