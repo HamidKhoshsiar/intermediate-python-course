b544964713a8f3656294aa2136184c821fb99fd7
Hamid Khoshsiar <59198898+HamidKhoshsiar@users.noreply.github.com>
2021-03-26 01:24:01 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,40 @@
-#A little Anormal Dice with unequal chance
 import random
+min = 1
+max = 7
+choices = []
 def main():
   #print('You rolled a die')
-  dice = random.randrange(1, 7)
+  dice = random.randrange(min, max)
+  print(dice)
+  choices.append(dice)
 
-  if dice == 6:
-    dice = random.randrange(1, 7)
-    print(dice)
+# if __name__== "__main__":
+#   main()
+def dice_size():
+  try:
+    min_changer = int(input('Define the minimum of dice'))
+    global min
+    min = min_changer
+  except:print('Only integer')
+  try:
+    max_changer = int(input('Define the maximum of dice'))
+    global max
+    max = max_changer + 1
+  except:print('Only integer')
+
+
+
+print(min)
+cntr = 0
+while cntr ==0:
+  user = input("roll the dice?(y/n)\n O for option")
+  if user == 'y' or user == 'Y':
+    main()
+  elif user == 'n' or user =='N':
+    cntr=1
+    print('the sum of all choices are: ',sum(choices))
+    print(choices)
+  elif user == 'o' or user == 'O':
+    dice_size()
   else:
-    print(dice)
-
-if __name__== "__main__":
-  main()
+    print('Yes or No or option please!')
